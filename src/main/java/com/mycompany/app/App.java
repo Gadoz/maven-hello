421dad7a7c9053ff2db2cf9a2415545e3af52430
Gadoz <demonkong@gmail.com>
2019-08-01 16:17:57 +0700

    add password param

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String passWord = "12345"
+        String passWord = "12345";
     }
 }
